51826054485dc8713a61df017f5c5b3d6fd8c378
Samyak Bakliwal <w3bcode@gmail.com>
2020-07-25 12:27:57 +0530

    Add products table

--- 1-create-table.sql
+++ 1-create-table.sql
@@ -10,16 +10,16 @@ CREATE TABLE `customers` (
   PRIMARY KEY (`customer_id`)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/* -- Create products table */
-/* DROP TABLE IF EXISTS `products`; */
-/* CREATE TABLE `products` ( */
-/*   `product_id` int(11) NOT NULL AUTO_INCREMENT, */
-/*   `product_name` varchar(200) NOT NULL, */
-/*   `current_stock` varchar(50) NOT NULL, */
-/*   `sellprice` int(11) NOT NULL, */
-/*   `buyprice` int(11) NOT NULL, */
-/*   PRIMARY KEY (`product_id`) */
-/*     ) ENGINE=InnoDB DEFAULT CHARSET=utf8; */ 
+ -- Create products table
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE `products` (
+  `product_id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(200) NOT NULL,
+  `current_stock` varchar(50) NOT NULL,
+  `sellprice` int(11) NOT NULL,
+  `buyprice` int(11) NOT NULL,
+  PRIMARY KEY (`product_id`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8; 
 
 /* -- create orders table */
 /* DROP TABLE IF EXISTS `orders`; */
