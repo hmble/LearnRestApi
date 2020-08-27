1966feaa62d10326a7fa9b7359441071a5ae92c5
Samyak Bakliwal <w3bcode@gmail.com>
2020-08-27 16:51:10 +0530

    client: Renderd all products list

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import { json } from "body-parser";
 
 function App() {
-  async function fetchData() {
-    const res = await fetch("/products/all");
-    const data = await res.json();
-    console.log(data);
-  }
-  fetchData();
+  const [data, setData] = useState(["hello"]);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await fetch("/products/all");
+        const data = await res.json();
+        setData(data);
+      } catch (err) {
+        console.log("got error ", err);
+      }
+    }
+    fetchData();
+  });
 
-  return <div className="App">{}</div>;
+  return (
+    <div className="App">
+      {data.map((d) => (
+        <div className="products">
+          <h3>{d.product_name}</h3>
+          <p>{d.sellprice}</p>
+          <p>{d.buyprice}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
